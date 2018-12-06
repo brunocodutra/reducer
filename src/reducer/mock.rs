5eab171715a9dd6016aa2f53312abc24fe67ad29
Bruno Dutra <brunocodutra@gmail.com>
2018-12-06 21:26:00 +0100

    define trait Reducer

---
+++ src/reducer/mock.rs
@@ -0,0 +1,38 @@
+#![cfg(test)]
+
+use reducer::Reducer;
+
+#[derive(Debug, Default, Clone, Eq, PartialEq)]
+pub struct MockReducer<A: 'static> {
+    actions: Vec<A>,
+}
+
+impl<A> MockReducer<A> {
+    pub fn new(actions: Vec<A>) -> Self {
+        Self { actions }
+    }
+}
+
+impl<A> Reducer for MockReducer<A> {
+    type Action = A;
+
+    fn reduce(&mut self, action: Self::Action) {
+        self.actions.push(action);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn reduce() {
+        let mut state = MockReducer::default();
+
+        state.reduce(5);
+        state.reduce(1);
+        state.reduce(3);
+
+        assert_eq!(state, MockReducer::new(vec![5, 1, 3]));
+    }
+}
---
+++ src/reducer/mod.rs
@@ -0,0 +1,29 @@
+mod mock;
+
+pub trait Reducer: 'static {
+    type Action;
+    fn reduce(&mut self, action: Self::Action);
+}
+
+#[cfg(test)]
+pub use self::mock::*;
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn reduce() {
+        let mut mock = MockReducer::default();
+
+        {
+            let state: &mut Reducer<Action = _> = &mut mock;
+
+            state.reduce(5);
+            state.reduce(1);
+            state.reduce(3);
+        }
+
+        assert_eq!(mock, MockReducer::new(vec![5, 1, 3]));
+    }
+}
