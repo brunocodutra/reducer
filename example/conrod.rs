684fd606f0ae1d7c6b7d3fe7c1224e59f0c719e0
Bruno Dutra <brunocodutra@gmail.com>
2021-01-31 16:51:39 +0100

    update and simplify the conrod based example

--- example/conrod.rs
+++ example/conrod.rs
@@ -1,19 +1,24 @@
 //! A simple example demonstrating how to implement a Todo List app using Reducer & Conrod.
 
-use conrod_core::*;
-use conrod_gfx::*;
-use conrod_winit::*;
-use futures::executor::*;
-use gfx::{format::DepthStencil, Device};
-use gfx_window_glutin::*;
-use glutin::dpi::LogicalSize;
-use glutin::Event::WindowEvent;
-use glutin::WindowEvent::{CloseRequested, Resized};
-use glutin::{ContextBuilder, EventsLoop, WindowBuilder};
-use reducer::*;
+use conrod_core::color::{Colorable, DARK_CHARCOAL, LIGHT_BLUE, WHITE};
+use conrod_core::position::{Positionable, Sizeable};
+use conrod_core::text::FontCollection;
+use conrod_core::widget::{text_box, Button, Canvas, List, TextBox, Toggle, Widget};
+use conrod_core::{image, widget_ids, Borderable, Labelable, UiBuilder, UiCell};
+use conrod_glium::Renderer;
+use conrod_winit::v023_conversion_fns;
+
+use glium::glutin::event::{Event, WindowEvent};
+use glium::glutin::event_loop::{ControlFlow, EventLoop};
+use glium::glutin::{dpi::LogicalSize, window::WindowBuilder, ContextBuilder};
+use glium::{Display, Surface};
+
+use std::time::{Duration, Instant};
+use std::{error::Error, mem, num::NonZeroUsize, sync::Arc};
+
+use futures::executor::{block_on, ThreadPool};
+use reducer::{Dispatcher, Reactor, Reducer, SpawnDispatcher, Store};
 use ring_channel::{ring_channel, RingReceiver};
-use std::{error::Error, mem, num::NonZeroUsize, sync::Arc, time::*};
-use winit::Window;
 
 #[derive(Debug, Copy, Clone, Eq, PartialEq)]
 enum View {
@@ -98,27 +103,10 @@ impl State {
     }
 }
 
-// A wrapper around the winit window that allows us to implement the trait necessary for enabling
-// the winit <-> conrod conversion functions.
-struct WindowRef<'a>(&'a Window);
-
-// Implement the `WinitWindow` trait for `WindowRef` to allow for generating compatible conversion
-// functions.
-impl<'a> WinitWindow for WindowRef<'a> {
-    fn get_inner_size(&self) -> Option<(u32, u32)> {
-        Window::get_inner_size(&self.0).map(Into::into)
-    }
-
-    fn hidpi_factor(&self) -> f32 {
-        Window::get_hidpi_factor(&self.0) as _
-    }
-}
-
-// Generate the winit <-> conrod_core type conversion fns.
-conversion_fns!();
-
 // Register widgets.
 widget_ids!(struct Ids { control, body, footer, button, input, list, all, done, pending });
+
+v023_conversion_fns!();
 
 // Renders the widgets given the current state.
 fn render<E: Error + 'static>(
@@ -128,35 +116,35 @@ fn render<E: Error + 'static>(
     dispatcher: &mut impl Dispatcher<Action, Output = Result<(), E>>,
 ) -> Result<(), Box<dyn Error>> {
     // Setup the layout.
-    widget::Canvas::new()
+    Canvas::new()
         .wh_of(ui.window)
         .pad(20.0)
         .pad_top(80.0)
         .pad_bottom(80.0)
         .middle_of(ui.window)
-        .color(color::DARK_CHARCOAL)
+        .color(DARK_CHARCOAL)
         .set(ids.body, ui);
 
-    widget::Canvas::new()
+    Canvas::new()
         .h(80.0)
         .pad(20.0)
         .pad_right(130.0)
         .mid_top_of(ui.window)
-        .color(color::DARK_CHARCOAL)
-        .border_color(color::DARK_CHARCOAL)
+        .color(DARK_CHARCOAL)
+        .border_color(DARK_CHARCOAL)
         .set(ids.control, ui);
 
-    widget::Canvas::new()
+    Canvas::new()
         .h(80.0)
         .w_of(ui.window)
         .pad(20.0)
         .mid_bottom_of(ui.window)
-        .color(color::DARK_CHARCOAL)
-        .border_color(color::DARK_CHARCOAL)
+        .color(DARK_CHARCOAL)
+        .border_color(DARK_CHARCOAL)
         .set(ids.footer, ui);
 
     // Render button to add a todo.
-    for _ in widget::Button::new()
+    for _ in Button::new()
         .top_right_with_margin_on(ui.window, 20.0)
         .w(100.0)
         .kid_area_h_of(ids.footer)
@@ -167,13 +155,13 @@ fn render<E: Error + 'static>(
     }
 
     // Render text input.
-    for input in widget::TextBox::new(state.get_input())
+    for input in TextBox::new(state.get_input())
         .mid_left_of(ids.control)
         .kid_area_wh_of(ids.control)
         .left_justify()
         .set(ids.input, ui)
     {
-        if let widget::text_box::Event::Update(text) = input {
+        if let text_box::Event::Update(text) = input {
             dispatcher.dispatch(Action::EditTodo(text))?;
         }
     }
@@ -181,7 +169,7 @@ fn render<E: Error + 'static>(
     let todos = state.get_todos();
 
     // Render the list of todos.
-    let (mut items, scrollbar) = widget::List::flow_down(todos.len())
+    let (mut items, scrollbar) = List::flow_down(todos.len())
         .item_size(40.0)
         .scrollbar_on_top()
         .middle_of(ids.body)
@@ -190,10 +178,10 @@ fn render<E: Error + 'static>(
 
     while let Some(item) = items.next(ui) {
         let (idx, done, todo) = todos[item.i];
-        let toggle = widget::Toggle::new(!done)
+        let toggle = Toggle::new(!done)
             .label(todo)
-            .label_color(color::WHITE)
-            .color(color::LIGHT_BLUE);
+            .label_color(WHITE)
+            .color(LIGHT_BLUE);
 
         for _ in item.set(toggle, ui) {
             dispatcher.dispatch(Action::ToggleTodo(idx))?;
@@ -206,7 +194,7 @@ fn render<E: Error + 'static>(
     }
 
     // Render button to show all todos.
-    let mut all = widget::Button::new()
+    let mut all = Button::new()
         .mid_left_of(ids.footer)
         .w(120.0)
         .kid_area_h_of(ids.footer)
@@ -214,7 +202,7 @@ fn render<E: Error + 'static>(
         .enabled(state.get_filter() != View::All);
 
     if state.get_filter() == View::All {
-        let color = color::WHITE.highlighted();
+        let color = WHITE.highlighted();
         all = all.color(color).hover_color(color).press_color(color);
     }
 
@@ -223,7 +211,7 @@ fn render<E: Error + 'static>(
     }
 
     // Render button to show only completed todos.
-    let mut done = widget::Button::new()
+    let mut done = Button::new()
         .middle_of(ids.footer)
         .w(120.0)
         .kid_area_h_of(ids.footer)
@@ -231,7 +219,7 @@ fn render<E: Error + 'static>(
         .enabled(state.get_filter() != View::Done);
 
     if state.get_filter() == View::Done {
-        let color = color::WHITE.highlighted();
+        let color = WHITE.highlighted();
         done = done.color(color).hover_color(color).press_color(color);
     }
 
@@ -240,7 +228,7 @@ fn render<E: Error + 'static>(
     }
 
     // Render button to show only pending todos.
-    let mut pending = widget::Button::new()
+    let mut pending = Button::new()
         .mid_right_of(ids.footer)
         .w(120.0)
         .kid_area_h_of(ids.footer)
@@ -248,7 +236,7 @@ fn render<E: Error + 'static>(
         .enabled(state.get_filter() != View::Pending);
 
     if state.get_filter() == View::Pending {
-        let color = color::WHITE.highlighted();
+        let color = WHITE.highlighted();
         pending = pending.color(color).hover_color(color).press_color(color);
     }
 
@@ -259,97 +247,67 @@ fn render<E: Error + 'static>(
     Ok(())
 }
 
-fn run_conrod<E: Error + 'static>(
-    mut dispatcher: impl Dispatcher<Action, Output = Result<(), E>>,
+fn run<E: Error + 'static>(
+    mut dispatcher: impl Dispatcher<Action, Output = Result<(), E>> + 'static,
     mut states: RingReceiver<Arc<State>>,
 ) -> Result<(), Box<dyn Error>> {
     const WIDTH: f64 = 400.;
     const HEIGHT: f64 = 500.;
 
-    let mut events_loop = EventsLoop::new();
+    let event_loop = EventLoop::new();
     let context = ContextBuilder::new();
-    let builder = WindowBuilder::new()
-        .with_dimensions((WIDTH, HEIGHT).into())
-        .with_min_dimensions((WIDTH, HEIGHT).into())
+    let window = WindowBuilder::new()
+        .with_inner_size(LogicalSize::<f64>::from((WIDTH, HEIGHT)))
+        .with_resizable(false)
         .with_title("Reducer <3 Conrod");
 
-    let (context, mut device, mut factory, rtv, _) =
-        init::<ColorFormat, DepthStencil>(builder, context, &events_loop)?;
+    let display = Display::new(window, context, &event_loop)?;
+    let mut renderer = Renderer::new(&display)?;
 
-    let mut encoder = factory.create_command_buffer().into();
-    let mut renderer = Renderer::new(&mut factory, &rtv, context.window().get_hidpi_factor())?;
-
-    let font = text::FontCollection::from_bytes(ttf_noto_sans::REGULAR)?.into_font()?;
+    let font = FontCollection::from_bytes(ttf_noto_sans::REGULAR)?.into_font()?;
     let mut ui = UiBuilder::new([WIDTH, HEIGHT]).build();
     ui.fonts.insert(font);
 
     let ids = Ids::new(ui.widget_id_generator());
-    let image_map = image::Map::new();
+    let image_map = image::Map::<glium::Texture2d>::new();
 
     // Keep a copy of the current state.
     let mut state = Arc::new(State::default());
 
-    // Point in time of the last refresh.
-    let mut rerendered_at = Instant::now();
+    event_loop.run(move |event, _, ctrl| {
+        // Stop the event loop when the window is closed.
+        if let Event::WindowEvent { ref event, .. } = event {
+            if let WindowEvent::CloseRequested = event {
+                *ctrl = ControlFlow::Exit;
+                return;
+            }
+        }
 
-    loop {
-        // If the window is closed, this will be None for one tick, so to avoid panicking with
-        // unwrap, instead break the loop
-        let LogicalSize { width, height } = match context.window().get_inner_size() {
-            Some(s) => s,
-            None => return Ok(()),
-        };
+        // Process window events.
+        if let Some(event) = convert_event(&event, &display.gl_window().window()) {
+            ui.handle_event(event);
+        }
+
+        // Update the current state.
+        if let Ok(next) = states.try_recv() {
+            state = next;
+        }
+
+        // Render widgets given the current state.
+        render(&mut ui.set_widgets(), &ids, &state, &mut dispatcher).unwrap();
 
         // Draw the `Ui` if it has changed.
         if let Some(primitives) = ui.draw_if_changed() {
-            let dpi_factor = context.window().get_hidpi_factor();
-            let dims = ((width * dpi_factor) as f32, (height * dpi_factor) as f32);
-
-            renderer.clear(&mut encoder, color::DARK_CHARCOAL.to_fsa());
-            renderer.fill(&mut encoder, dims, dpi_factor, primitives, &image_map);
-            renderer.draw(&mut factory, &mut encoder, &image_map);
-            encoder.flush(&mut device);
-            context.swap_buffers()?;
-            device.cleanup();
+            renderer.fill(&display, primitives, &image_map);
+            let mut target = display.draw();
+            target.clear_color(0.0, 0.0, 0.0, 1.0);
+            renderer.draw(&display, &mut target, &image_map).unwrap();
+            target.finish().unwrap();
         }
 
-        let mut exit = false;
-
-        events_loop.poll_events(|event| {
-            if let WindowEvent { ref event, .. } = event {
-                if let CloseRequested = event {
-                    exit = true;
-                }
-
-                if let Resized(logical_size) = event {
-                    let hidpi_factor = context.window().get_hidpi_factor();
-                    let physical_size = logical_size.to_physical(hidpi_factor);
-                    context.resize(physical_size);
-                    let (new_color, _) = new_views::<ColorFormat, DepthStencil>(&context);
-                    renderer.on_resize(new_color);
-                }
-            }
-
-            if let Some(event) = convert_event(event, &WindowRef(context.window())) {
-                ui.handle_event(event);
-            }
-        });
-
-        if exit {
-            return Ok(());
-        }
-
-        // Render at 60fps.
-        if Instant::now().duration_since(rerendered_at) > Duration::from_millis(16) {
-            if let Ok(next) = states.try_recv() {
-                state = next;
-            }
-
-            // Render widgets given the current state.
-            render(&mut ui.set_widgets(), &ids, &state, &mut dispatcher)?;
-            rerendered_at = Instant::now();
-        }
-    }
+        // Render at ~60fps.
+        *ctrl = ControlFlow::WaitUntil(Instant::now() + Duration::from_millis(16));
+    });
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
@@ -369,7 +327,7 @@ fn main() -> Result<(), Box<dyn Error>> {
     let (dispatcher, handle) = executor.spawn_dispatcher(store)?;
 
     // Run the rendering loop.
-    run_conrod(dispatcher, rx)?;
+    run(dispatcher, rx)?;
 
     // Wait for the background thread to complete.
     block_on(handle)?;
