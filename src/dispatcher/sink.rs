f992292e48d338301c5add01e5852c706a7836ab
Bruno Dutra <brunocodutra@gmail.com>
2021-08-21 17:51:19 +0200

    remove Reactor::from_sink and Dispatcher::from_sink

--- src/dispatcher/sink.rs
+++ src/dispatcher/sink.rs
@@ -1,23 +1,39 @@
 use crate::dispatcher::*;
-use derive_more::{Deref, DerefMut};
+use derive_more::{Deref, DerefMut, From};
 use futures::executor::block_on;
 use futures::sink::{Sink, SinkExt};
-use pin_project::*;
+use pin_project::pin_project;
 use std::pin::Pin;
 use std::task::{Context, Poll};
 
-/// A handle that allows dispatching actions on a [spawned] [`Dispatcher`] (requires [`async`]).
+/// An adapter for [`Sink`]s that behaves as an asynchronous [`Dispatcher`] (requires [`async`])
 ///
-/// This type is a just lightweight handle that may be cloned and sent to other threads.
-///
-/// [spawned]: Store::into_task
 /// [`async`]: index.html#optional-features
+///
+/// # Example
+/// ```rust
+/// use reducer::*;
+/// use futures::channel::mpsc::channel;
+/// use futures::executor::block_on_stream;
+/// use std::thread;
+///
+/// let (tx, rx) = channel(0);
+/// let mut dispatcher = AsyncDispatcher(tx);
+///
+/// thread::spawn(move || {
+///     dispatcher.dispatch(1);
+///     dispatcher.dispatch(1);
+///     dispatcher.dispatch(2);
+///     dispatcher.dispatch(3);
+///     dispatcher.dispatch(5);
+///     dispatcher.dispatch(8);
+/// });
+///
+/// assert_eq!(block_on_stream(rx).collect::<Vec<u8>>(), [1, 1, 2, 3, 5, 8]);
+/// ```
 #[pin_project]
-#[derive(Debug, Default, Copy, Clone, Eq, PartialEq, Hash, Deref, DerefMut)]
-pub struct AsyncDispatcher<T> {
-    #[pin]
-    sink: T,
-}
+#[derive(Debug, Default, Copy, Clone, Eq, PartialEq, Hash, From, Deref, DerefMut)]
+pub struct AsyncDispatcher<T>(#[pin] pub T);
 
 impl<A, T> Dispatcher<A> for AsyncDispatcher<T>
 where
@@ -43,53 +59,19 @@ where
     type Error = T::Error;
 
     fn poll_ready(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
-        self.project().sink.poll_ready(cx)
+        self.project().0.poll_ready(cx)
     }
 
     fn start_send(self: Pin<&mut Self>, action: A) -> Result<(), Self::Error> {
-        self.project().sink.start_send(action)
+        self.project().0.start_send(action)
     }
 
     fn poll_flush(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
-        self.project().sink.poll_flush(cx)
+        self.project().0.poll_flush(cx)
     }
 
     fn poll_close(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
-        self.project().sink.poll_close(cx)
-    }
-}
-
-impl<A, E> dyn Dispatcher<A, Output = Result<(), E>> {
-    /// Adapts any type that implements [`Sink`] as a [`Dispatcher`] (requires [`async`]).
-    ///
-    /// [`async`]: index.html#optional-features
-    ///
-    /// # Example
-    /// ```rust
-    /// use reducer::*;
-    /// use futures::channel::mpsc::channel;
-    /// use futures::executor::block_on_stream;
-    /// use std::thread;
-    ///
-    /// let (tx, rx) = channel(0);
-    /// let mut dispatcher = Dispatcher::<_, Output = _>::from_sink(tx);
-    ///
-    /// thread::spawn(move || {
-    ///     dispatcher.dispatch(1);
-    ///     dispatcher.dispatch(1);
-    ///     dispatcher.dispatch(2);
-    ///     dispatcher.dispatch(3);
-    ///     dispatcher.dispatch(5);
-    ///     dispatcher.dispatch(8);
-    /// });
-    ///
-    /// assert_eq!(block_on_stream(rx).collect::<Vec<u8>>(), vec![1, 1, 2, 3, 5, 8]);
-    /// ```
-    pub fn from_sink<T>(sink: T) -> AsyncDispatcher<T>
-    where
-        T: Sink<A, Error = E> + Unpin,
-    {
-        AsyncDispatcher { sink }
+        self.project().0.poll_close(cx)
     }
 }
 
@@ -103,7 +85,7 @@ mod tests {
     proptest! {
         #[test]
         fn deref(mut sink: Vec<u8>) {
-            let mut dispatcher = <dyn Dispatcher<_, Output = _>>::from_sink(sink.clone());
+            let mut dispatcher = AsyncDispatcher(sink.clone());
 
             assert_eq!(dispatcher.deref(), &sink);
             assert_eq!(dispatcher.deref_mut(), &mut sink);
@@ -118,7 +100,7 @@ mod tests {
                 .times(1)
                 .return_const(result);
 
-            let mut dispatcher = <dyn Dispatcher<_, Output = _>>::from_sink(mock);
+            let mut dispatcher = AsyncDispatcher(mock);
             assert_eq!(Dispatcher::dispatch(&mut dispatcher, action), result);
         }
 
@@ -131,7 +113,7 @@ mod tests {
                 .times(1)
                 .return_const(result);
 
-            let mut dispatcher = <dyn Dispatcher<_, Output = _>>::from_sink(mock);
+            let mut dispatcher = AsyncDispatcher(mock);
             assert_eq!(block_on(dispatcher.send(action)), result);
             assert_eq!(block_on(dispatcher.close()), Ok(()));
         }
